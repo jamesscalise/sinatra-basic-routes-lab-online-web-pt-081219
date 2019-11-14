f34cbc14120b4f1acac234b5bbd63e661dad160e
James Scalise <jamescscalise@gmail.com>
2019-11-14 21:16:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is James"
+  end
+  
+  get '/hometown' do
+    "My hometown is Cary"
+  end
+  
+  get '/favorite-song' do
+  end
 end
